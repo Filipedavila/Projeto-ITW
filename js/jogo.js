496c2a1a11945a25bd5b0b56d59c75074958c473
unknown <filipedavila6@gmail.com>
2022-05-08 16:35:33 +0100

    Update

--- js/jogo.js
+++ js/jogo.js
@@ -1,10 +1,86 @@
 /* ------------------------------------------------------------------------- */
 
-/** Tempo do jogo */
-const TEMPO_JOGO = "tempoJogo";
+/* ------------------------Game Screens IDS--------------------------- */
+
+/** Tempo do jogo SINGLE PLAYER e QuickGame */
+const ID_SPAN_TEMPO_JOGO = "idScreenTempoJogo";
+
+/**  Times Won SINGLE PLAYER Information **/
+
+const ID_TIMES_WON_SINGLE_PLAYER= "timesWonPlayer";
+
+/**  Times Won MULTIPLAYER P1 Information **/
+
+const ID_TIMES_WON_MULTIPLAYER_P1= "timesWonPlayer1";
+
+/**  Times Won MULTIPLAYER P2 Information **/
+
+const ID_TIMES_WON_MULTIPLAYER_P2= "timesWonPlayer2";
+
+/**  ID points game SinglePlayer **/
+
+const ID_POINTS_SINGLEPLAYER= "idPointsSinglePlayer";
+
+/**  ID points game Multiplayer P1 **/
+
+const ID_POINTS_MULTIPLAYER_P1= "idPointsPlayer1";
+
+/**  ID points game Multiplayer P2 **/
+
+const ID_POINTS_MULTIPLAYER_P2= "idPointsPlayer2";
+
+/**  id Sound Control Game **/
+
+const ID_SOUND_CONTROL ="soundControl";
+
 
 /** Intervalo do tempo do jogo */
 var temporizadorTempoJogo;
+
+
+
+/* ------------------------Game Buttons--------------------------- */
+
+/** Restart Game SinglePlayer do tempo do jogo */
+
+const BTN_ID_RESET_GAME_SP = "smile";
+
+/** Restart Game Multiplayer player 1 */
+
+const BTN_ID_RESET_GAME_MP_P1 = "smileP1";
+
+/** Restart Game Multiplayer Player 2 */
+
+const BTN_ID_RESET_GAME_MP_P2 = "smileP2";
+
+
+
+
+
+/* ------------------------Game Tables--------------------------- */
+
+/** Game Table SINGLE PLAYER */
+
+const ID_TABLE_SINGLEPLAYER = "idGameTable";
+
+/** Game Table SINGLE PLAYER */
+
+const ID_TABLE_MULTIPLAYER_P1 = "idGameTableP1";
+
+
+/** Game Table SINGLE PLAYER */
+
+const ID_TABLE_MULTIPLAYER_P2  = "idGameTableP2";
+
+
+
+
+
+
+
+
+
+
 
 /* ------------------------------------------------------------------------- */
 
@@ -62,12 +138,47 @@ const NOME_AVATAR_2 = 'nomeAvatar2';
 const IMAGEM_AVATAR_2 = 'imagemAvatar2';
 
 /* ------------------------------------------------------------------------- */
-
+var jogoCurrente;
 /** Estado do jogo, que vai sendo preenchido no decorrer do jogo. */
-let jogo = {
-  
-    inicio: null
-  
+var jogoSp = {
+    table_player:null,
+    inicio: null,
+    points:0,
+    times_won_player1:0,
+    difficulty:null,
+    size:null,
+    addPointsP1: function(points){  this.points_player1 += points;},
+    addTimeWonP1: function(){  this.times_won_player1++;},
+    resetGame:function (){
+        this.points_player1 = 0;
+        this.points_player2= 0},
+    init:function (){
+
+    }
+
+
+};
+/** Estado do jogo Multiplayer, que vai sendo preenchido no decorrer do jogo. */
+var jogoMp = {
+    table_player1:null,
+    table_player2:null,
+    name_player1:null,
+    name_player2:null,
+    inicio: null,
+    points_player1:0,
+    points_player2:0,
+    times_won_player1:0,
+    times_won_player2:0,
+    difficulty:null,
+    size:null,
+    addPointsP1: function(points){  this.points_player1 += points;},
+    addPointsP2: function(points){  this.points_player2 += points;},
+    addTimeWonP1: function(){  this.times_won_player1++;},
+    addTimeWonP2: function(){  this.times_won_player2++ ;},
+    resetGame:function (){
+        this.points_player1 = 0;
+        this.points_player2= 0}
+
 };
 
 /* ------------------------------------------------------------------------- */
@@ -107,7 +218,7 @@ function recolheDadosConfiguracaoSingleplayer() {
 /** Recolhe os dados da configuração para um jogo multiplayer e guarda-os na
  * local storage
  */
- function recolheDadosConfiguracaoMultiplayer() {
+function recolheDadosConfiguracaoMultiplayer() {
 
     let formulario = document.forms[FORMULARIO_MULTIPLAYER];
 
@@ -147,25 +258,27 @@ function recolheDadosConfiguracaoSingleplayer() {
  */
 
 function configuracaoSingleplayer() {
+    if(window.localStorage.getItem(ITEM_CONFIGURACAO_S) != null) {
 
-    configuracao = JSON.parse(localStorage.getItem(ITEM_CONFIGURACAO_S)) || [];
+        configuracao = JSON.parse(localStorage.getItem(ITEM_CONFIGURACAO_S)) || [];
 
-    let tamanho = configuracao[1];
+        let tamanho = configuracao[1];
 
-    if (tamanho == 'tamanhoEasy') {
-        init(9,9);
+        if (tamanho == 'tamanhoEasy') {
+            init(9, 9);
+        } else if (tamanho == 'tamanhoMedium') {
+            init(16, 16);
+        } else if (tamanho == 'tamanhoHard') {
+            init(30, 16);
+        }
+
+        document.getElementById(NOME_AVATAR_S).innerHTML = configuracao[0];
+
+        // Falta configurar a imagem
+    }else{
+        window.location.replace("index.html");
     }
-    else if (tamanho == 'tamanhoMedium') {
-        init(16,16);
-    }
-    else if (tamanho == 'tamanhoHard') {
-        init(30,16);
-    }
 
-    document.getElementById(NOME_AVATAR_S).innerHTML = configuracao[0];
-
-    // Falta configurar a imagem
-    
 
 }
 
@@ -175,123 +288,112 @@ function configuracaoSingleplayer() {
  * caso multiplayer
  */
 
- function configuracaoMultiplayer() {
+function configuracaoMultiplayer() {
+    if(window.localStorage.getItem(ITEM_CONFIGURACAO_M) != null) {
 
-    configuracaoM = JSON.parse(localStorage.getItem(ITEM_CONFIGURACAO_M)) || [];
+        configuracaoM = JSON.parse(localStorage.getItem(ITEM_CONFIGURACAO_M)) || [];
 
-    let nomesAvatar1 = document.getElementsByClassName(NOME_AVATAR_1);
+        let nomesAvatar1 = document.getElementsByClassName(NOME_AVATAR_1);
 
-    for (let i = 0; i < nomesAvatar1.length; i++) {
-        nomesAvatar1[i].innerHTML = configuracaoM[0];
+        for (let i = 0; i < nomesAvatar1.length; i++) {
+            nomesAvatar1[i].innerHTML = configuracaoM[0];
+        }
+
+        let nomesAvatar2 = document.getElementsByClassName(NOME_AVATAR_2);
+
+        for (let i = 0; i < nomesAvatar2.length; i++) {
+            nomesAvatar2[i].innerHTML = configuracaoM[1];
+        }
+
+        let tamanho = configuracaoM[2];
+
+
+        if (tamanho == 'tamanhoEasy') {
+            initMulti(9, 9);
+        } else if (tamanho == 'tamanhoMedium') {
+            initMulti(16, 16);
+        } else if (tamanho == 'tamanhoHard') {
+            initMulti(30, 16);
+        }
+
+
+
+    }else{
+        window.location.replace("index.html");
     }
-
-    let nomesAvatar2 = document.getElementsByClassName(NOME_AVATAR_2);
-
-    for (let i = 0; i < nomesAvatar2.length; i++) {
-        nomesAvatar2[i].innerHTML = configuracaoM[1];
-    }
-
-    let tamanho = configuracaoM[2];
-
-
-    /**
-     * nao funciona o tamanho
-     * problema com initMulti
-     * Uncaught TypeError: Cannot read properties of undefined (reading 'cells')
-     */
-
-    if (tamanho == 'tamanhoEasy') {
-        initMulti(9,9);
-    }
-    else if (tamanho == 'tamanhoMedium') {
-        initMulti(16,16);
-    }
-    else if (tamanho == 'tamanhoHard') {
-        initMulti(30,16);
-    }
-
-    // Falta configurar a imagem
-    
-
 }
 
 /* ------------------------------------------------------------------------- */
 
 
 
-//TO DO , FUNÇÕES QUE RECEBAM AS OPÇÕES DOS JOGOS DO HTML
 
 const openedCellSound = new Audio('../audio/open.mp3');
 
-var table;
 
-/**TO DO , ligar o init as definições do user
- * TEREMOS QUE ARRANJAR MANEIRA TAMBÉM DE PASSAR TODOS OS DADOS
- * CRIAR LIGAÇÕES ENTRE CERTAS PARTES DO HTML PARA APRESENTAR O NOME DO JOGADOR,
- * O SCORE DO JOGADOR , E NO CASO DE MULTIPLAYER FUNÇÕES EXTRA
+
+/**Função que inicia o jogo Singular
  *
- * @param colunas
- * @param linhas
+ * //TO DO  distribuir de forma random as bombas no jogo
+ *
+ * @param colunas - numero de colunas do jogo
+ * @param linhas  - numero de Linhas do jogo
  */
 function init(colunas = 8, linhas = 8){
-     this.table  =  new Table(colunas,linhas);
-console.log("criou table");
-var localJogo = document.getElementById("table");
+    jogoCurrente = jogoSp;
+    jogoSp.table_player  =  new Table(colunas,linhas);
+    console.log("criou table");
+    let localJogo = document.getElementById(ID_TABLE_SINGLEPLAYER);
 
 // criar table
-var tabela = document.createElement('table');
-tabela.setAttribute('class' ,'gameTable');
-tabela.setAttribute('alt','Janela do Jogo');
+    let tabela = document.createElement('table');
+    tabela.setAttribute('class' ,'gameTable');
+    tabela.setAttribute('alt','Janela do Jogo');
 
 // faz cada linha
-for(i=1;i<=linhas; i++ ){
- var linha =document.createElement('tr');
- linha.setAttribute('class' ,'gameRow');
+    for(let i=1;i<=linhas; i++ ){
+        let linha =document.createElement('tr');
+        linha.setAttribute('class' ,'gameRow');
 
-    //Criar Celulas do jogo
-    for(j = 1 ; j <= colunas;j++){
-        var celula = document.createElement('td');
-        let row = String(i);
-        let col = String(j);
-        let id = row + col ;
-        celula.setAttribute('class','celula');
-        celula.setAttribute('id', id);
-        celula.addEventListener("mouseup",clicado,false);
-       table.cells[i][j].buttonTd = celula;
-        linha.appendChild(celula);
-        
+        //Criar Celulas do jogo
+        for(j = 1 ; j <= colunas;j++){
+            let celula = document.createElement('td');
+            let row = String(i);
+            let col = String(j);
+            let id = row +","+ col ;
+            celula.setAttribute('class','celula');
+            celula.setAttribute('id', id);
+            celula.setAttribute('alt','Celula nº '  + "("+id+")");
+            celula.addEventListener("mouseup",clicadoSp,false);
+            jogoSp.table_player.cells[i][j].buttonTd = celula;
+            linha.appendChild(celula);
+
         }
 
 
-  tabela.appendChild(linha);
+        tabela.appendChild(linha);
+    }
+
+    localJogo.appendChild(tabela);
+
+    // Marca o inicio do tempo de jogo
+    jogoSp.inicio = Math.floor(Date.now() / 1000);
+
+    // Chama mostraTempoJogo() a cada segundo
+    temporizadorTempoJogo = setInterval(mostraTempoJogo, 1000);
+
+
 }
 
-localJogo.appendChild(tabela);
 
-  // Marca o inicio do tempo de jogo
-  jogo.inicio = Math.floor(Date.now() / 1000);
-
-  // Chama mostraTempoJogo() a cada segundo
-  temporizadorTempoJogo = setInterval(mostraTempoJogo, 1000);
-
-
-}
-
-
-/** Mostra o tempo do jogo */
+/** Mostra o tempo do jogo
+ *
+ * Função chamada pelo setinterval que atualiza o contador de tempo do jogo no sitio pretendido
+ *
+ */
 function mostraTempoJogo() {
     var zeroPad = (num, places) => String(num).padStart(places, '0')
-    document.getElementById(TEMPO_JOGO).innerHTML = zeroPad(Math.floor((Date.now()/1000)-jogo.inicio), 3);
-}
-
-/**
- * Colocar na funcao que terminar o jogo
- * Cancela o tempo do jogo
- * clearInterval(temporizadorTempoJogo)
- */
-
-function startGame(){
-
+    document.getElementById(ID_SPAN_TEMPO_JOGO).innerHTML = zeroPad(Math.floor((Date.now()/1000)-jogoCurrente.inicio), 3);
 }
 
 /**
@@ -300,89 +402,99 @@ function startGame(){
  * @param linhas
  */
 function initMulti(colunas = 8, linhas = 8){
+    jogoCurrente = jogoMp;
+    jogoMp.table_player1  =  new Table(Number(colunas),Number(linhas));
+    jogoMp.table_player2 = new Table(colunas,linhas);
+    console.log("criou table");
 
-  var localJogo = document.getElementById("game1");
-  var localJogo2 = document.getElementById("game2");
+    var localJogo = document.getElementById(ID_TABLE_MULTIPLAYER_P1);
+    var localJogo2 = document.getElementById(ID_TABLE_MULTIPLAYER_P2);
 
-  var tabela = document.createElement('table');
-  tabela.setAttribute('class' ,'gameTable');
-  tabela.setAttribute('alt','Jogador 1');
-  
-  // faz cada linha
-  for(i=1;i<=linhas; i++ ){
-   var linha =document.createElement('tr');
-   linha.setAttribute('class' ,'gameRow');
-      //faz cada celula
-      for(j = 1 ; j <= colunas;j++){
-          var celula = document.createElement('td');
-          
-          celula.setAttribute('class','celula');
-          celula.setAttribute('id', i*10+j);
-          celula.setAttribute('alt','Celula nº ' + i*10+j);
-          celula.addEventListener("click",clicado,false);
-          // por o elemento da celula diretamente na abstração da celula
-          table.cells[linha][colunas].setTdElement(celula);
-          
-          linha.appendChild(celula);
-          
-          } 
-          
-    tabela.appendChild(linha);
-  }
-  
-  localJogo.appendChild(tabela);
+    let tabela = document.createElement('table');
+    tabela.setAttribute('class' ,'gameTable');
+    tabela.setAttribute('alt','Jogador 1');
 
-  var tabela2 = document.createElement('table');
-  tabela2.setAttribute('class' ,'gameTable');
-  tabela2.setAttribute('alt','Jogador 2');
-  
-  // faz cada linha
-  for(i=0;i<linhas; i++){
-   var linha2 =document.createElement('tr');
-   linha2.setAttribute('class' ,'gameRow');
-   linha2.setAttribute('alt','Linha ' + i);
-      //faz cada celula
-      for(j = 0 ; j < colunas;j++){
-          var celula2 = document.createElement('td');
-          let row = String(i);
-          let col = String(j);
-          let id = row + col ;
-          celula2.setAttribute('class','celula');
-          celula2.setAttribute('id',  id);
-          celula2.setAttribute('alt','Celula nº ' + id);
-          celula2.addEventListener("mouseup",function(e){
-              e.preventDefault();
-              clicado(e);    },false);
-          
-          linha2.appendChild(celula2);
-          
-          } 
-          
-    tabela2.appendChild(linha2);
-  }
-  localJogo2.appendChild(tabela2);
-  }
+    // faz cada linha
+    for(let i=1;i<=linhas; i++ ){
+        let linha =document.createElement('tr');
+        linha.setAttribute('class' ,'gameRow');
+        //faz cada celula
+        for(let j = 1 ; j <= colunas;j++){
+            var celulaMp1 = document.createElement('td');
+            let row = String(i);
+            let col = String(j);
+            let id ="P1," +row +","+ col ;
+            celulaMp1.setAttribute('class','celula');
+            celulaMp1.setAttribute('id', id);
+            celulaMp1.setAttribute('alt','Player 1 Celula nº ' + "("+id+")");
+            celulaMp1.addEventListener("mouseup",clicadoMp,false);
+            // por o elemento da celula diretamente na abstração da celula
+            jogoMp.table_player1.cells[i][j].setTdElement(celulaMp1);
+
+            linha.appendChild(celulaMp1);
+
+        }
+
+        tabela.appendChild(linha);
+    }
+
+    localJogo.appendChild(tabela);
+
+    var tabela2 = document.createElement('table');
+    tabela2.setAttribute('class' ,'gameTable');
+    tabela2.setAttribute('alt','Jogador 2');
+
+    // faz cada linha
+    for(let i=1;i<=linhas; i++){
+        var linha2 =document.createElement('tr');
+        linha2.setAttribute('class' ,'gameRow');
+        linha2.setAttribute('alt','Linha ' + i);
+        //faz cada celula
+        for(let j = 1 ; j <= colunas;j++){
+            var celulaMp2 = document.createElement('td');
+            let row = String(i);
+            let col = String(j);
+            let id ="P2," + row +","+ col ;
+            celulaMp2.setAttribute('class','celula');
+            celulaMp2.setAttribute('id',  id);
+            celulaMp2.setAttribute('alt','Player 2 Celula nº '  + "("+id+")");
+            celulaMp2.addEventListener("mouseup",clicadoMp,false);
+            jogoMp.table_player2.cells[i][j].setTdElement(celulaMp2);
+
+            linha2.appendChild(celulaMp2);
+
+        }
+
+        tabela2.appendChild(linha2);
+    }
+    localJogo2.appendChild(tabela2);
+    jogoMp.inicio = Math.floor(Date.now() / 1000);
+
+    // Chama mostraTempoJogo() a cada segundo
+    temporizadorTempoJogo = setInterval(mostraTempoJogo, 1000);
+}
 
 
-/**
+/**Função responsavel pelo o evento que ocorre quando uma celula do jogo é premida
  *
  * @param e
  */
-function clicado(e){
+function clicadoSp(e){
     openedCellSound.play();
-     // manda abrir o opencell e espera retorno, dependendo do retorno ira fazer uma acção no jogo
-    var t = e.target;
+    // manda abrir o opencell e espera retorno, dependendo do retorno ira fazer uma acção no jogo
+    let t = e.target;
 
-   let id = t.id;
-   let row = id.charAt(0);
-   let col = id.charAt(1);
-console.log(e.button )
+    let id = t.id.split(",");
+
+    let row = id[0];
+    let col = id[1];
+    console.log(e.button )
 
     if(e.button == 0) {
-        table.cells[row][col].openCell();
+        jogoSp.table_player.cells[row][col].openCell();
     }
     if(e.button ==2){
-        table.cells[row][col].placeFlag();
+        jogoSp.table_player.cells[row][col].placeFlag();
     }
     e.preventDefault();
     e.stopPropagation();
@@ -390,6 +502,41 @@ console.log(e.button )
 
 
 }
+
+/**Função responsavel pelo o evento que ocorre quando uma celula do jogo é premida
+ *
+ * @param e
+ */
+function clicadoMp(e){
+    var currentPlayer;
+    openedCellSound.play();
+    // manda abrir o opencell e espera retorno, dependendo do retorno ira fazer uma acção no jogo
+    let t = e.target;
+
+    let id = t.id.split(",");
+    if(id[0] == "P1"){
+        currentPlayer = jogoMp.table_player1;
+    }else if(id[0] == "P2"){
+        currentPlayer = jogoMp.table_player2;
+
+    }
+    let row = id[1];
+    let col = id[2];
+    console.log(e.button )
+
+    if(e.button == 0) {
+        currentPlayer.cells[row][col].openCell();
+    }
+    if(e.button ==2){
+        currentPlayer.cells[row][col].placeFlag();
+    }
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+
+
+}
+
 
 /**Classe que representa o tabuleiro do jogo que contem todos as celulas ou seja butões do Jogo
  * , é responsavel por manipular a lógica do jogo.
@@ -414,12 +561,12 @@ class Table {
 
         this.cells = new Array(row);
         for(var i = 1 ; i<= row ; i++){
-                this.cells[i] =new Array(col);
+            this.cells[i] =new Array(col);
         }
 
         for(var i = 1 ; i <= col ; i++){
             for(var j = 0; j< row; j++){
-            this.cells[i][j] =new Cell(i,j);
+                this.cells[i][j] =new Cell(i,j);
             }
 
         }
@@ -444,7 +591,7 @@ class Table {
         console.log(j);
         console.log(i);
 
-            /// REVER LOGICA DOS ADJACENTES
+        /// REVER LOGICA DOS ADJACENTES
         if(i>=2 ) this.cells.adjCells.push(this.cells[i-1][j]);
         if(j>=2)  this.cells.adjCells.push(this.cells[i][j-1]);
         if(i< this.cells.size) this.cells.adjCells.push(this.cells[i][j+i]);
@@ -521,7 +668,7 @@ class Cell {
             //this.openAdj();
             console.log("Aberta Celula [" + this.row + "," + this.col + "]");
             this.buttonTd.setAttribute('class', 'openedCell');
-            this.buttonTd.removeEventListener("mouseup",clicado);
+            this.buttonTd.removeEventListener("mouseup",clicadoSp);
             this.placeNumber();
         }
     }
@@ -542,14 +689,14 @@ class Cell {
      *
      */
     openAdj(){
-    for(let i = 0; i< 4 ; i++){
-        if(this.adjCells[i] != undefined) {
-            if (this.adjCells[i].hasBomb() == 1) {
-                this.nearBombNumber = +1;
-            }else{
-                this.adjCells[i].openCell();
+        for(let i = 0; i< 4 ; i++){
+            if(this.adjCells[i] != undefined) {
+                if (this.adjCells[i].hasBomb() == 1) {
+                    this.nearBombNumber = +1;
+                }else{
+                    this.adjCells[i].openCell();
+                }
             }
-        }
         }
 
 
